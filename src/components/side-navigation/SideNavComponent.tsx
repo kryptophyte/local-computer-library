b8c7e2dcf6cd4daf6561cf9e0400762b35fd73c0
kryptophyte <prokhorlovestea@gmail.com>
2023-11-28 14:15:33 +0300

    added list of books

--- src/components/side-navigation/SideNavComponent.tsx
+++ src/components/side-navigation/SideNavComponent.tsx
@@ -1,6 +1,15 @@
 import React from "react";
 import {books} from "../../books";
+import {ListMember} from "../../editors/ListMember/ListMember"
+import '../../App.css';
 
-class SideNavComponent extends React.PureComponent {
-    
+export class SideNavComponent extends React.PureComponent {
+
+    render() {
+        return (
+            <div className={'sideNav'}>
+                    <ListMember/>
+            </div>
+        );
+    }
 }
